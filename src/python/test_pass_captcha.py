463a3f044878abb9285968990d5aebfa3fe3e397
Xavier Barinaga <aganirabx@gmail.com>
2025-11-08 16:42:20 -0800

    Get Function now takes parameters

--- src/python/test_pass_captcha.py
+++ src/python/test_pass_captcha.py
@@ -1,12 +1,13 @@
 import requests, random, time
-import random
 from bs4 import BeautifulSoup
+
+import gui
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 
-def human_get_selenium():
+def human_get_selenium(query, website, wait=False):
     options = Options()
     options.add_argument(
         "Mozilla/5.0 (Linux; Android 14; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.105 Mobile Safari/537.36"
@@ -23,64 +24,36 @@ def human_get_selenium():
     options.add_argument("--disable-popup-blocking")
     #options.add_argument("--enable-javascript")
     prefs = {"profile.default_content_setting_values.notifications": 2}
-    options.add_argument("--headless")
     options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(options=options)
 
     # options.add_argument("--headless")
     # options.add_argument("--disable-gpu")
-    # driver = webdriver.Chrome(options=options)
-    encoded_query = "bike" #<-- here
-    driver.get(f"https://globalesearch.com/search/shoes?sortBy=BestMatch&searchInDescription=false&se=0")
-    html = driver.page_source  # full rendered HTML
-
-    soup = BeautifulSoup(html, "html.parser")
-    pretty_html = soup.prettify()
-
-    with open("../../output/pre_parsed_html/ebay.html", "w", encoding="utf-8") as f:
-        f.write(pretty_html)
-
-    driver.quit()
-
-def human_get_selenium_wait():
-    options = Options()
-    options.add_argument(
-        "Mozilla/5.0 (Linux; Android 14; Pixel 7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.6261.105 Mobile Safari/537.36"
-    )
-    # Adding argument to disable the AutomationControlled flag
-    options.add_argument("--disable-blink-features=AutomationControlled")
-
-    # Exclude the collection of enable-automation switches
-    options.add_experimental_option("excludeSwitches", ["enable-automation"])
-
-    # Turn-off userAutomationExtension
-    options.add_experimental_option("useAutomationExtension", False)
-    options.add_argument("--disable-notifications")
-    options.add_argument("--disable-popup-blocking")
-    #options.add_argument("--enable-javascript")
-    prefs = {"profile.default_content_setting_values.notifications": 2}
-    #options.add_argument("--headless")
-    options.add_experimental_option("prefs", prefs)
     driver = webdriver.Chrome(options=options)
 
-    options.add_argument("--disable-gpu")
-
-    encoded_query = "bike"
-    driver.get(f"https://globalesearch.com/search/shoes?sortBy=BestMatch&searchInDescription=false&se=0")
-
-    time.sleep(5)
+    
+    
+    if website == "amazon":
+        driver.get(f"https://www.amazon.com/s?k={query}")
+    if website == "ebay":
+        driver.get(f"https://globalesearch.com/search/{query}") # se=0 means the search will be for Ebay United States
+        time.sleep(5) # Waiting for website to load
+    if website == "target":
+        driver.get(F"https://www.target.com/s?searchTerm={query}")
+    
 
     html = driver.page_source  # full rendered HTML
-
+    
     soup = BeautifulSoup(html, "html.parser")
     pretty_html = soup.prettify()
 
-    with open("../../output/pre_parsed_html/ebay.html", "w", encoding="utf-8") as f:
+    with open(f"../../output/pre_parsed_html/{website}.html", "w", encoding="utf-8") as f:
         f.write(pretty_html)
 
     input("Press Enter to close browser...")
 
     driver.quit()
+
 
 def human_get_requests(url, session=None, headers=None, min_delay=1.5, max_delay=4.0, timeout=10):
     if session is None:
@@ -111,4 +84,4 @@ def human_get_requests(url, session=None, headers=None, min_delay=1.5, max_delay
 
 # human_get_selenium()
 
-human_get_selenium_wait()
+human_get_selenium("Black Nike Shoes", "ebay", wait=True)
